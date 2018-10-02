a796a7a3963c531a08ae4aa0b98737d0dc208e2a
Pornmongkon Pongsai <pornmongkonp@gmail.com>
2018-10-02 09:15:49 +0700

    [Add] Test input 1 should return 1

---
+++ fizzbuzz_test.go
@@ -0,0 +1,16 @@
+package fizzbuzz
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	tests := []struct {
+		testcase, number, expected string
+	}{
+		{"input 1 should 1", "1", "1"},
+	}
+	for _, test := range tests {
+		if result := FizzBuzz(test.number); result != test.expected {
+			t.Fatalf("%s FizzBuzz(%s) = %v, want %v", test.testcase, test.number, result, test.expected)
+		}
+	}
+}
